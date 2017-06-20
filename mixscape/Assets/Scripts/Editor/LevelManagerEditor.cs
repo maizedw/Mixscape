9e0d5fed4d5d0b23acedb5fb8dd31af2c41c72b3
Russell Dilley <russell.dilley@gmail.com>
2017-06-20 17:28:11 +1000

    Adding Free Rocks and Skybox.

---
+++ mixscape/Assets/Scripts/Editor/LevelManagerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelManager), true)]
+public class LevelManagerEditor : Editor
+{
+    public LevelManager TargetLevelManager { get { return target as LevelManager; } }
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Change Skybox"))
+        {
+            TargetLevelManager.NextSkybox();
+        }
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ mixscape/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LevelManager : MonoBehaviour
+{
+    public Material[] SkyboxMaterials;
+    private int CurrentSkybox;
+    
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < SkyboxMaterials.Length; ++i)
+        {
+            if (RenderSettings.skybox == SkyboxMaterials[i])
+            {
+                CurrentSkybox = i;
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void NextSkybox()
+    {
+        if (++CurrentSkybox >= SkyboxMaterials.Length)
+        {
+            CurrentSkybox = 0;
+        }
+        RenderSettings.skybox = SkyboxMaterials[CurrentSkybox];
+#if UNITY_EDITOR
+        UnityEditor.SceneManagement.EditorSceneManager.MarkAllScenesDirty();
+#endif
+    }
+}
