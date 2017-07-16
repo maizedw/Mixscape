73fb4c1b0f0e6287295af99a4237bd0d6f971593
Jason Bakker <jason.bakker@gmail.com>
2017-07-16 13:58:30 +1000

    Improving character placement, fixing up rhino turning (they shouldn't fall over any more), making treasure boingable

--- mixscape/Assets/Scripts/Creature.cs
+++ mixscape/Assets/Scripts/Creature.cs
@@ -7,6 +7,8 @@ public class Creature : MonoBehaviour
     public float TurnSpeedDegrees = 180.0f;
 
     private Animator _animator;
+    private float _desiredRotationDegrees;
+
     public Animator Animator
     {
         get
@@ -26,25 +28,63 @@ public class Creature : MonoBehaviour
     {
     }
 
+    protected virtual void Update()
+    {
+        if(Mathf.Abs(_desiredRotationDegrees) > 0.0f)
+        {
+            float thisFrameRotation = TurnSpeedDegrees * Time.deltaTime * Mathf.Sign(_desiredRotationDegrees);
+            if(Mathf.Abs(thisFrameRotation) > Mathf.Abs(_desiredRotationDegrees))
+            {
+                thisFrameRotation = _desiredRotationDegrees;
+                _desiredRotationDegrees = 0.0f;
+            }
+            else
+            {
+                _desiredRotationDegrees -= thisFrameRotation;
+            }
+
+            transform.RotateAround(transform.position, transform.up, thisFrameRotation);
+        }
+    }
+
     public void TurnToFace(Vector3 direction)
     {
         if(direction == Vector3.zero)
             return;
-
-        LeanTween.cancelAll(gameObject);
-
-        Vector3 currForward = transform.forward;
-        currForward.Normalize();
-        Vector3 targetForward = direction;
-        targetForward.y = 0.0f;
-        targetForward.Normalize();
-        float forwardDot = Vector3.Dot(currForward, targetForward);
-        if(forwardDot < 1.0f)
+        Vector3 currFwd = transform.forward;
+        currFwd.y = 0.0f;
+        currFwd.Normalize();
+        float currRotationDot = Vector3.Dot(currFwd, Vector3.forward);
+        float currRotation = Mathf.Rad2Deg * Mathf.Acos(currRotationDot);
+        if(Vector3.Dot(currFwd, Vector3.left) > 0.0f)
         {
-            float angleToTarget = Mathf.Rad2Deg * Mathf.Acos(forwardDot);
-            float time = angleToTarget / TurnSpeedDegrees;
-            Quaternion desiredFacingRotation = Quaternion.LookRotation(targetForward);
-            LeanTween.rotate(gameObject, desiredFacingRotation.eulerAngles, time);
+            currRotation = -currRotation;
+        }
+
+        Vector3 targetFwd = direction;
+        targetFwd.y = 0.0f;
+        targetFwd.Normalize();
+        float targetRotationDot = Vector3.Dot(targetFwd, Vector3.forward);
+        float targetRotation = Mathf.Rad2Deg * Mathf.Acos(targetRotationDot);
+        if(Vector3.Dot(targetFwd, Vector3.left) > 0.0f)
+        {
+            targetRotation = -targetRotation;
+        }
+
+        if(Mathf.Approximately(currRotation, targetRotation) == false)
+        {
+            _desiredRotationDegrees = targetRotation - currRotation;
+            if(Mathf.Abs(_desiredRotationDegrees) > 180.0f)
+            {
+                if(_desiredRotationDegrees < 0.0f)
+                {
+                    _desiredRotationDegrees += 360.0f;
+                }
+                else if(_desiredRotationDegrees > 0.0f)
+                {
+                    _desiredRotationDegrees -= 360.0f;
+                }
+            }
         }
     }
 }
