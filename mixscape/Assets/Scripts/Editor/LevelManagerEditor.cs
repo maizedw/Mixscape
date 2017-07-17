215151720eb91bb3cdd9928872ce94e158d9e7df
Russell Dilley <russell.dilley@gmail.com>
2017-07-17 22:37:55 +1000

    Moved some level settings from LevelManager into LevelSettings.asset. Added button to LevelManager to select Terrain object.

--- mixscape/Assets/Scripts/Editor/LevelManagerEditor.cs
+++ mixscape/Assets/Scripts/Editor/LevelManagerEditor.cs
@@ -8,13 +8,85 @@ public class LevelManagerEditor : Editor
 {
     public LevelManager TargetLevelManager { get { return target as LevelManager; } }
 
+    private LevelInfo _levelInfo = null;
+    private int _currentSkybox;
+    
+    void OnEnable()
+    {
+        LoadLevelSettings();
+    }
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
+        EditorGUILayout.Separator();
+
+        if (GUILayout.Button("Select Terrain"))
+        {
+            Terrain terrain = TargetLevelManager.transform.GetComponentInChildren<Terrain>();
+            if (terrain == null) terrain = GameObject.FindObjectOfType<Terrain>();
+
+            if (terrain != null)
+            {
+                Selection.activeGameObject = terrain.gameObject;
+            }
+        }
+
         if (GUILayout.Button("Set to Next Skybox"))
         {
-            TargetLevelManager.NextSkybox();
+            NextSkybox();
         }
+
+    }
+
+    public void LoadLevelSettings()
+    {
+        _levelInfo = Resources.Load<LevelInfo>("LevelSettings");
+        if (_levelInfo == null)
+        {
+            _levelInfo = new LevelInfo();
+            AssetDatabase.CreateAsset(_levelInfo, "Assets/Resources/LevelSettings.asset");
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        InitSkyboxSettings();
+    }
+
+    public void InitSkyboxSettings()
+    {
+        if (_levelInfo == null) return;
+
+        for (int i = 0; i < _levelInfo.SkyboxMaterials.Length; ++i)
+        {
+            if (RenderSettings.skybox == _levelInfo.SkyboxMaterials[i])
+            {
+                _currentSkybox = i;
+                break;
+            }
+        }
+    }
+
+    public void NextSkybox()
+    {
+        if (_levelInfo == null) return;
+
+        if (++_currentSkybox >= _levelInfo.SkyboxMaterials.Length)
+        {
+            _currentSkybox = 0;
+        }
+
+        if (_currentSkybox < _levelInfo.SkyboxMaterials.Length)
+        {
+            RenderSettings.skybox = _levelInfo.SkyboxMaterials[_currentSkybox];
+
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                UnityEditor.SceneManagement.EditorSceneManager.MarkAllScenesDirty();
+            }
+        }
+#endif
     }
 }
